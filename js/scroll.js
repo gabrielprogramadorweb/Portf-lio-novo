506bdfc5593a4916fe362481d8c00428787691fa
gabrielprogramadorads <gabriel.programador.ads@gmail.com>
2022-11-17 09:45:31 -0300

    att

---
+++ js/scroll.js
@@ -0,0 +1,19 @@
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('.layer__header', { duration: 3000});
+
+sr.reveal('.layer layers__front', {  duration: 3000});
+sr.reveal('.main-article', {  duration: 3000});
+sr.reveal('.sobre', {  duration: 3000});
+
+
+sr.reveal('.nave', { 
+    rotate: { x: 0, y: 0, z: 0},
+    duration:2000
+
+});
+
+sr.reveal('.menu-icon', { 
+    rotate:{ x: 100, y: 100, z: 0},
+    duration:2500,
+});
